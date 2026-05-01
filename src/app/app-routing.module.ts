1b92903c469a49df8df602f1513c3fcba06b9aa6
Pawel Jaworski <pawel.jaworski-extern@dbschenker.com>
2026-05-01 09:47:23 +0200

    deployment fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import {GnomeGameComponent} from "./gnome-game/gnome-game.component";
 
 const routes: Routes = [
+  { path: '', redirectTo: 'gnome-game', pathMatch: 'full' },
   { path: 'gnome-game', component: GnomeGameComponent }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
